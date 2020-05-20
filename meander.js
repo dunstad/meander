5b1f71e732aa085429c7015a2a69d71cb8456e04
dunstad <anthony00.blount00@gmail.com>
2020-05-20 22:26:41 +0900

    buttons and scenes work

--- meander.js
+++ meander.js
@@ -1,1 +1,70 @@
 console.log('script working!')
+
+var game = {};
+
+game.scenes = {
+
+  one: {
+    image: 'test.png',
+    buttons: [
+      {
+        text: 'hello hello hello',
+        next: 'two',
+        color: 'pastel-green'
+      },
+      {
+        text: 'world',
+        next: 'one',
+        color: 'pastel-orange'
+      },
+    ],
+  },
+  
+  two: {
+    image: 'test2.png',
+    buttons: [
+      {
+        text: 'hello again',
+        next: 'one',
+        color: 'pastel-green'
+      },
+      {
+        text: 'world again',
+        next: 'two',
+        color: 'pastel-orange'
+      },
+    ],
+  },
+
+};
+
+function loadScene(scene) {
+
+  document.getElementById('art').src = scene.image;
+
+  let choices = document.getElementById('choices');
+  
+  while (choices.firstChild) {
+    choices.removeChild(choices.firstChild);
+  }
+  
+  for (let butt of scene.buttons) {
+    let div = document.createElement('div');
+    div.classList.add('col-12');
+    
+    let button = document.createElement('button');
+    button.classList.add('btn', butt.color);
+    button.textContent = butt.text;
+    button.onclick = ()=>{loadScene(game.scenes[butt.next]);};
+    
+    div.appendChild(button);
+    choices.appendChild(div);
+  }
+
+}
+
+function main() {
+  loadScene(game.scenes.one);
+}
+
+main();
